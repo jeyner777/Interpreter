6ae0079b2400ffc5b628b7feaaff8d6b8da4e97b
Oscar Méndez <osjomete@gmail.com>
2021-03-26 08:24:05 -0600

    conds, setq, functions and write

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -73,6 +73,7 @@ public class Interpreter {
 						case "multiply":  return multiply(code);
 						case "/":  return divide(code);
 						case "divide":  return divide(code);
+						case "write": return write(code);
 						case "setq":  return setq(code);
 						case "atom":  return atom(code);
 						case "list":  return list(code);
@@ -80,12 +81,12 @@ public class Interpreter {
 						case "=": return equal(code);
 						case ">":  return greater(code);
 						case "<":  return less(code);
-						case "cond":  return cond(code);
+						case "cond":  return cond(code, 0);
 						default: return operator;
 					}	
 				}
 			} else if (variables.contains(code)) {
-				return code;
+				return "variables.get("+ code + ")";
 			} else {
 				try {
 					Double d = Double.parseDouble(code);
@@ -98,7 +99,7 @@ public class Interpreter {
 			throw e;
 		}
 	}
-	
+
 	/**
 	 * Splits parameters
 	 * @param code
@@ -204,16 +205,62 @@ public class Interpreter {
 		return "";
 	}
 	
-	private String setq(String code) {
-		return "";
+	public String setq(String code) throws Exception {
+		ArrayList<String> parameters = separate(code, 4);
+		
+		//Revisa si tiene la cantidad correcta de parametros
+		if(parameters.size() < 2) {
+			throw new Exception("La definicion de variables con 'setq' requiere de dos parametros");
+		} else if (parameters.size() > 2) {
+			throw new Exception("La definicion de variables con 'setq' requiere unicamente de dos parametros");
+		}
+		
+		String variable_name = parameters.get(0).toLowerCase();
+		String value = parameters.get(1);
+		
+		if(variable_name != variable_name.split(" ")[0].split("\\(")[0].toLowerCase()) {
+			throw new Exception("Nombre de variable incorrecto al utilizar 'setq'");
+		} else if(variable_name.equals("t")) {
+			throw new Exception("La variable 't' sesta reservada, no puede utilizarse como nombre de variable con 'setq'");
+		} else if(variable_name.equals("nil")) {
+			throw new Exception("La variable 'nil' sesta reservada, no puede utilizarse como nombre de variable con 'setq'");
+		}
+		
+		if(variables.contains(variable_name)) {
+			return "variables.replace(" + variable_name + ", " + translate(value) + ")";
+		}
+		
+		variables.add(variable_name);
+		
+		return "variables.put(" + variable_name + ", new ReturnValue(" + translate(value) + "))";
 	}
 	
 	public String defun(String code) {
 		return "";
 	}
 	
-	private String function(String code) {
-		return code;
+	public String function(String code) throws Exception {
+		ArrayList<String> parameters = separate(code, 0);
+		String function_name = parameters.get(0);
+		String java_syntax = function_name + "(";
+		
+		if(parameters.size() >= 2) {
+			try {
+				java_syntax += translate(parameters.get(1));
+			} catch (Exception e) {
+				throw e;
+			}
+		}
+		
+		for(int i=2; i<parameters.size(); i++) {
+			try {
+				java_syntax += ", " + translate(parameters.get(i));
+			} catch (Exception e) {
+				throw e;
+			}
+		}
+		
+		return java_syntax + ")";
 	}
 	
 	private String atom(String code) {
@@ -222,6 +269,14 @@ public class Interpreter {
 	
 	private String list(String code) {
 		return "";
+	}
+	
+	private String write(String code) throws Exception {
+		ArrayList<String> parameters = separate(code, 5);
+		if(parameters.size() != 1) {
+			throw new Exception("La funcion 'write' solo puede tener un parametro.");
+		}
+		return "System.out.println(" + translate(parameters.get(0)) + ")";
 	}
 	
 	public String equal(String code) throws Exception {
@@ -282,84 +337,135 @@ public class Interpreter {
 		return java_syntax;
 	}
 	
-	private String cond(String code) throws Exception {
-		ArrayList<String> parameters = separate(code, 4);
-		String java_syntax = "\tprivate static RetunValue cond" + (conds.size()-1) + "() {\n";
+	public String cond(String code, int indentation_level) throws Exception {
+		String indent = "";
+		for(int i=0; i<indentation_level; i++) {
+			indent += "\t";
+		}
 		
-		ArrayList<String> test_actions;
-		String last_expression, operator;
-		boolean exists;
-		for(String parameter : parameters) {
-			exists = false;
-			parameter = parameter.substring(1, parameter.length()-1);
-			test_actions = separate(parameter, 0);
-			if(parameters.indexOf(parameter) == 0) {
-				if(test_actions.get(0).charAt(0) == '(') {
-					try {
-						java_syntax += "\t\tif(" + translate(test_actions.get(0)) + ") {\n";
-					} catch (Exception e) {
-						throw e;
-					}
-				} else if(test_actions.get(0).toLowerCase().equals("t") || test_actions.get(0).toLowerCase().equals("nil")) {
-					java_syntax += "\t\tif(" + test_actions.get(0) + ") {\n";
-				} else {
-					for(String variable : variables) {
-						if(variable.equals(test_actions.get(0))) {
-							exists = true;
-						}
-					}
-					if(exists) {
-						java_syntax += "\t\tif(" + test_actions.get(0) + ") {\n";
-					} else {
-						throw new Exception("Los tests de las condicionales deben ser booleanos.");
-					}
-				} 
-			} else {
-				if(test_actions.get(0).charAt(0) == '(') {
-					try {
-						java_syntax += " else if(" + translate(test_actions.get(0)) + ") {\n";
-					} catch (Exception e) {
-						throw e;
-					}
-				} else if(test_actions.get(0).toLowerCase().equals("t") || test_actions.get(0).toLowerCase().equals("nil")) {
-					java_syntax += " telse if(" + test_actions.get(0) + ") {\n";
-				} else {
-					for(String variable : variables) {
-						if(variable.equals(test_actions.get(0))) {
-							exists = true;
-						}
-					}
-					if(exists) {
-						java_syntax += "\t\tif(" + test_actions.get(0) + ") {\n";
-					} else {
-						throw new Exception("Los tests de las condicionales deben ser booleanos.");
-					}
-				} 
-			}
+		ArrayList<String> parameters = separate(code, 4);
+		//Se obtienene la primera condici[on, con su test y sus acciones.
+		ArrayList<String> test_actions = separate(parameters.get(0).substring(1, parameters.get(0).length()-1), 0);
+		String test = test_actions.get(0), java_syntax;
+		if (isReturn(code)) {
+			int pos = conds.size();
+			String cond_name = "cond" + (pos+1) + "()";
+			conds.add(cond_name);
+			java_syntax = "private static ReturnValue() " + cond_name + " {\n";
 			
-			if(test_actions.size() == 2) {
+			for(int i=0; i<parameters.size(); i++) {
+				parameters = separate(code, 4);
+				test_actions = separate(parameters.get(i).substring(1, parameters.get(i).length()-1), 0);
+				//Se obtiene el tests de la condicion
+				test = test_actions.get(0);
 				
-			} else {
-				for(int i=1; i<test_actions.size()-1; i++) {
-					if(test_actions.get(0).charAt(0) == '(') {
-						operator = test_actions.get(i).split(" ")[0].split("\\(")[0].toLowerCase();
-						if(operator.equals("setq") || operator.equals("write")) {
+				//Se verifica si es if o if else.
+				if(i == 0) {
+					java_syntax += indent + "if(" + translate(test) + ") {\n";
+				} else {
+					java_syntax += indent + "else if(" + translate(test) + ") {\n";
+				}
+				
+				//Se recorren todas las acciones de la condicion.
+				for(int j=1; j<test_actions.size(); j++) {
+					String action = test_actions.get(j), action2;
+					if(action.charAt(0) == '(') {
+						action2 = action.substring(1, action.length()-1);
+						String operator = action2.split(" ")[0].split("\\(")[0].toLowerCase(); 
+						if(operator.equals("cond")) {
 							try {
-								java_syntax += "\t\t\t" + translate(test_actions.get(i)) + ";\n";
+								if(j == test_actions.size()-1) {
+									java_syntax += indent + "\treturn ";
+								}
+								java_syntax += cond(action2, indentation_level) + ";\n";
+								continue;
 							} catch (Exception e) {
 								throw e;
 							}
 						}
 					}
+					
+					try {
+						if(j == test_actions.size()-1) {
+							java_syntax += indent + "\treturn ";
+							if(isReturnValue(action)) {
+								java_syntax += translate(action);
+							} else {
+								java_syntax += "new ReturnValue(" + translate(action) + ");\n";
+							}
+						} else {
+							java_syntax += indent + "\t" + translate(action) + ";\n";
+						}
+					} catch (Exception e) {
+						throw e;
+					}
 				}
-				last_expression = test_actions.get(test_actions.size()-1);
-				operator = last_expression.split(" ")[0].split("\\(")[0].toLowerCase();
-				if(operator.equals("setq") || operator.equals("write")) {
-					//java_syntax = "\t\t\t" + translate();
+				java_syntax += indent + "}\n";
+			}
+			
+			//Se guarda el string de la funcion condicional, pero no se retorna.
+			conds.set(pos, java_syntax); 
+			
+			//Se retorna el nombre generado de la funcion condicional
+			return cond_name;
+		} else {
+			//ArrayList<String> parameters = separate(code, 4);
+			//Se obtienene la primera condici[on, con su test y sus acciones.
+			//ArrayList<String> test_actions = separate(parameters.get(0).substring(1, parameters.get(0).length()-1), 0);
+			//String test = test_actions.get(0);
+			java_syntax = indent + "if(" + translate(test) + ") {\n";
+			for(int i=1; i<test_actions.size(); i++) {
+				String action = test_actions.get(i), action2;
+				if(action.charAt(0) == '(') {
+					action2 = action.substring(1, action.length()-1);
+					String operator = action2.split(" ")[0].split("\\(")[0].toLowerCase(); 
+					if(operator.equals("cond")) {
+						try {
+							java_syntax += cond(action2, indentation_level+1);
+							continue;
+						} catch (Exception e) {
+							throw e;
+						}
+					}
+				}
+				try {
+					java_syntax += indent + "\t" + translate(action) + ";\n";
+				} catch (Exception e) {
+					throw e;
 				}
 			}
+			java_syntax += indent + "}\n";
+			
+			for(int i=1; i<parameters.size(); i++) {
+				parameters = separate(code, 4);
+				test_actions = separate(parameters.get(i).substring(1, parameters.get(i).length()-1), 0);
+				test = test_actions.get(0);
+				java_syntax += indent + "else if(" + translate(test) + ") {\n";
+				for(int j=1; j<test_actions.size(); j++) {
+					String action = test_actions.get(j), action2;
+					if(action.charAt(0) == '(') {
+						action2 = action.substring(1, action.length()-1);
+						String operator = action2.split(" ")[0].split("\\(")[0].toLowerCase(); 
+						if(operator.equals("cond")) {
+							try {
+								java_syntax += cond(action2, indentation_level+1);
+								continue;
+							} catch (Exception e) {
+								throw e;
+							}
+						}
+					}
+					try {
+						java_syntax += indent + "\t" + translate(action) + ";\n";
+					} catch (Exception e) {
+						throw e;
+					}
+				}
+				java_syntax += indent + "}\n";
+			}
+			
+			return java_syntax;
 		}
-		return "cond" + conds.size() + "()";
 	}
 	
 	/**
@@ -371,37 +477,35 @@ public class Interpreter {
 	public boolean isReturn(String code, String...function_name) {
 		String operator, last_action;
 		//Se verifica si es cond, ya que estas pueden tener retorno o no.
-		if(code.substring(0, 4).equals("cond")) {
-			ArrayList<String> parameters = separate(code, 4), test_action;
-			System.out.println(parameters);
-			for(String parameter : parameters) {
-				parameter = parameter.substring(1, parameter.length()-1);
-				test_action = separate(parameter, 0);
-				last_action = test_action.get(test_action.size()-1);
-				//Se evalua el caso en que el cond tenga la misma funcion de donde fue llamada como ultima accion.
-				if(last_action.charAt(0) == '(') {
-					last_action = last_action.substring(1, last_action.length()-1);
-					operator = last_action.split(" ")[0].split("\\(")[0].toLowerCase();
-					System.out.println(operator);
-					if(function_name.length > 0) {
-						if(!operator.equals(function_name[0]) && isReturn(last_action, function_name[0])) {
+		if(code.length() >= 4) {
+			if(code.substring(0, 4).equals("cond")) {
+				ArrayList<String> parameters = separate(code, 4), test_action;
+				for(String parameter : parameters) {
+					parameter = parameter.substring(1, parameter.length()-1);
+					test_action = separate(parameter, 0);
+					last_action = test_action.get(test_action.size()-1);
+					//Se evalua el caso en que el cond tenga la misma funcion de donde fue llamada como ultima accion.
+					if(last_action.charAt(0) == '(') {
+						last_action = last_action.substring(1, last_action.length()-1);
+						operator = last_action.split(" ")[0].split("\\(")[0].toLowerCase();
+						if(function_name.length > 0) {
+							if(!operator.equals(function_name[0]) && isReturn(last_action, function_name[0])) {
+								return true;
+							}
+						} else if(isReturn(last_action)) {
 							return true;
 						}
-					} else if(isReturn(last_action)) {
-						System.out.println(last_action);
+					} else {
 						return true;
 					}
-				} else {
-					return true;
 				}
+				return false;
 			}
-			return false;
 		}
 	
 		operator = code.split(" ")[0].split("\\(")[0].toLowerCase();
-		System.out.println(operator);
 		//Se verifica si es uno de los tres metodos que nunca tienen retorno.
-		if(operator.equals("setq") || operator.equals("write") || operator.equals("write-line")) {
+		if(operator.equals("setq") || operator.equals("write")) {
 			return false;
 		//Si es una funcion definida por el usuaurio, se verifica si esta tiene retorno.
 		} else if (function_name.length > 0) {
@@ -416,5 +520,33 @@ public class Interpreter {
 	
 		//Si no es ningun de las posibles funciones sin retorno
 		return true;
+	}
+	
+	public boolean isReturnValue(String code) {
+		if(code.charAt(0) == '\'') {
+			code = code.substring(2, code.length()-1);
+			return false;
+		} else if (code.charAt(0) == '('){
+			code = code.substring(1, code.length()-1);
+			String operator = code.split(" ")[0].split("\\(")[0].toLowerCase();
+			if (functions.containsKey(operator) || operator.equals("cond")) {
+				return true;
+			} else {
+				return false;
+			}
+		} else if (variables.contains(code)) {
+			return true;
+		} else {
+			try {
+				Double d = Double.parseDouble(code);
+			} catch (Exception e) {
+				return true;
+			}
+			return false;
+		}
+	}
+	
+	public ArrayList<String> getConds() {
+		return conds;
 	}
 }
