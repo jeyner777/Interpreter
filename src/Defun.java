d7e70630b847a2b7e40890528814175659cf440f
jeyner777 <jeyner777@gmail.com>
2021-04-05 18:43:16 -0500

    3er intento defun

--- src/Defun.java
+++ src/Defun.java
@@ -2,12 +2,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Defun {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		System.out.println("Prueba");
 	
-		// script de main
-		String code = "defun FaCT (n a b c &key f g &optional h) " +
+		// Script de main
+		String code = "defun sqr (x) (* x x)";
+		String code2 = "defun FaCT (z a b c &key f g &optional h) " +
 				"(if (<= n 0)" + 
 				"1" + 
 				"(* n (fact (1- n))))";
@@ -27,11 +28,17 @@ public class Defun {
 		String function = "\n\n" + tabString + "public static ReturnValue "+ parts.get(0).toLowerCase() + "(";
 		parts.remove(0); // removes the name of the function from the ArrayList
 		tabString += "\t";
-		ArrayList<String> arguments = unpack(parts.get(0));
-		System.out.println(parts); // borrar
+		ArrayList<String> arguments = new ArrayList<>();
+		
+		//Check if only one or more parameters on the function
+		if(parts.get(0).length() <= 3 ) {
+			arguments.add(Character.toString(parts.get(0).charAt(1)));
+		} else {
+			arguments = reader.separate(parts.get(0).substring(1, parts.get(0).length()-1),0);
+		}
+		
 		arguments.removeIf(n -> n.equals("&key")); // erases '&key' for Lisp mandatory assign values
 		arguments.removeIf(n -> n.equals("&optional")); // erases '&optional' for Lisp optional values
-		System.out.println(arguments); // borrar
 		
 		// Writes down parameter values
 		for (String argument: arguments) {
@@ -39,6 +46,8 @@ public class Defun {
 		}
 		function += "){\n";
 		parts.remove(0); // Removes the arguments of the function
+		
+		System.out.println(function); // borrar
 		System.out.println(parts); // borrar
 		
 		//Check for Operators to translate easy to define functions
@@ -47,100 +56,66 @@ public class Defun {
 				"=", "/=", ">", "<", ">=", "<=", "max", "min", "equal", "greater", "less", // Comparison Operations
 				"and", "or", "not", "xor", //Logical Operations on Boolean Values
 				"setq", "atom", "list", "setf", "cond")); // S-Expressions
-		for(String i: parts) { // cambiar a normal loop
-			ArrayList<String> instruction = unpack(i);
+		
+		//Determine type of function
+		//1. Es un ReturnValue, una variable o una funcion con retorno.
+		//2. Es un retorno normal, como una suma o resta 
+		//3. No tiene retorno, comp si es un print o un setq
+		
+		String value = parts.get(parts.size()-1);
+		if(reader.isReturn(value)) {
+			for(int i =0; i < parts.size()-2; i++) {
+				function += reader.translate(parts.get(i));
+			}
+			function += tabString + reader.translate(parts.get(parts.size()-1)) + ";\n" ;
+		} else if(reader.isReturnValue(value)) {
+			for(int i =0; i < parts.size()-2; i++) {
+				function += reader.translate(parts.get(i));
+			}
+			function += tabString + reader.translate(parts.get(parts.size()-1)) + ";\n" ;
+		} else {
+			for(int i =0; i < parts.size()-2; i++) {
+				function += reader.translate(parts.get(i));
+			}
+			function += tabString + reader.translate(parts.get(parts.size()-1)) + ";\n" ;
+		}
+		
+		System.out.println(function);
+
+		// aqui seria de volarme esto
+		/*
+		for(String i: parts) {
+			ArrayList<String> instruction = reader.separate(i.substring(1, i.length()-1),0);
 			System.out.println(instruction); //Borrar
-			if (operators.contains(instruction.get(0))){ //finds if argument is a common operator
-				function += tabString + reader.translate(i.substring(1, i.length()-1)) + ";\n";
-			} else if (instruction.get(0).toLowerCase().equals("if")) {
-				function += tabString + "if (" + reader.translate(instruction.get(1).substring(1, instruction.get(1).length()-1)) + ") {\n";
-				tabString += "\t";
-				function += tabString + instruction.get(2) + ";\n";
-				tabString = tabString.substring(0, tabString.length() - 1);
-				function += tabString + "} else {\n";
-				tabString += "\t";
-				function += tabString + reader.translate(instruction.get(3)) + ";\n";
-				tabString = tabString.substring(0, tabString.length() - 1);
-				function += tabString + "}";
-				tabString = tabString.substring(0, tabString.length() - 1);
+			if(parts.indexOf(i) < parts.size()-1) {
+				if (operators.contains(instruction.get(0))){ //finds if argument is a common operator
+					function += tabString + reader.translate(i.substring(1, i.length()-1)) + ";\n";
+				} else {
+					function += tabString + reader.translate(i.substring(1, i.length()-1)) + ";\n";
+					;
+				}
 			} else {
-				;
+				function += tabString + reader.translate(i.substring(1, i.length()-1)) + ";\n";	
 			}
 		}
+		*/
+		
 		function += "\n" + tabString + "}";
 		System.out.println(function); // borrar
 	}
 	
-	private static ArrayList<String> unpack(String code) {
-		code = code.substring(1, code.length()-1);
-		int count1 = 0, count2 = 0;
-		Integer[] range = new Integer[2];
-		ArrayList<Integer[]> ranges = new ArrayList<>();
-		
-		//Se encuentran los rangos de posiciones donde estan los parametros en el string
-		for(int i = 0; i < code.length(); i++) {
-			if(count1 == count2) {
-				if(code.charAt(i) == '(') {
-					if(i != 0) {
-						if(code.charAt(i-1) != '\'') {
-							range[0] = i;
-						}
-					} else {
-						range[0] = i;
-					}
-					count1++;
-				} else if(code.charAt(i) == '\'') {
-					range[0] = i;
-				} else if(code.charAt(i) != ' ') { 
-					if(i == code.length()-1) {
-						if(code.charAt(i-1) != ')' && code.charAt(i-1) != ' ') {
-							range[1] = i;
-							ranges.add(range);
-							range = new Integer[2];
-						} else {
-							range[0] = i;
-							range[1] = i;
-							ranges.add(range);
-							range = new Integer[2];
-						}
-					} else if(i == 0) {
-						range[0] = i;
-						if(code.charAt(i+1) == '(' || code.charAt(i+1) == ' ') {
-							range[1] = i;
-							ranges.add(range);
-							range = new Integer[2];
-						}
-					} else {
-						if(code.charAt(i-1) == ')' || code.charAt(i-1) == ' ') {
-							range[0] = i;
-						}	
-						if(code.charAt(i+1) == '(' || code.charAt(i+1) == ' '){
-							range[1] = i;
-							ranges.add(range);
-							range = new Integer[2];
-						}
-					}
+	/* 
+	 * else if (instruction.get(0).toLowerCase().equals("if")) {
+					function += tabString + "if (" + reader.translate(instruction.get(1).substring(1, instruction.get(1).length()-1)) + ") {\n";
+					tabString += "\t";
+					function += tabString + instruction.get(2) + ";\n";
+					tabString = tabString.substring(0, tabString.length() - 1);
+					function += tabString + "} else {\n";
+					tabString += "\t";
+					function += tabString + reader.translate(instruction.get(3)) + ";\n";
+					tabString = tabString.substring(0, tabString.length() - 1);
+					function += tabString + "}";
+					tabString = tabString.substring(0, tabString.length() - 1);
 				}
-			} else if(code.charAt(i) == '(') {
-				count1++;
-			} else if(code.charAt(i) == ')') {
-				count2++;
-				if(count1 == count2) {
-					range[1] = i;
-					ranges.add(range);
-					range = new Integer[2];
-				}
-			}
-		}
-		
-		//Se genera un ArrayList para los parametros de la funcion
-		ArrayList<String> parameters = new ArrayList<>();
-		String parameter;
-		for(Integer[] pair : ranges) {
-			//Se utiliza los rangos de posiciones encontrados anteriormente
-			parameter = code.substring(pair[0], pair[1]+1);
-			parameters.add(parameter);
-		}
-		return parameters;
-	}
+	 */
 }
